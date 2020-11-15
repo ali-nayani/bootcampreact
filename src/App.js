0092e602b884d2c7bcd0c645daad5035f522d1b7
ali nayani <ali_nayaniz@hotmail.com>
2020-11-15 23:06:35 +0500

    retested using yarn

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. tested through github
+          Edit <code>src/App.js</code> and save to reload. retested through github
         </p>
         <a
           className="App-link"
